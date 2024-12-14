5acca8a6381a9aedcad94cb15cc12265f8df0c99
daisuke289 <daisuke289@gmail.com>
2024-12-14 17:48:36 +0900

    ensure_correct_user を destroy アクションにも適用

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user).all
